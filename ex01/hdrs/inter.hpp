2a34abce25d05a370882a6613a1e45afd5696cdd
cartoone <contact@cartoone.dev>
2025-09-17 23:50:10 +0200

    ex01

--- ex01/hdrs/inter.hpp
+++ ex01/hdrs/inter.hpp
@@ -6,22 +6,22 @@
 /*   By: jramiro <jramiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:38:00 by jramiro           #+#    #+#             */
-/*   Updated: 2025/09/16 22:18:14 by jramiro          ###   ########.fr       */
+/*   Updated: 2025/09/17 23:49:55 by jramiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTER_HPP
 #define INTER_HPP
 
-template <typename T>
-void	iter(T* array, std::size_t array_len, void (*f)(T&))
+template <typename T, std::size_t N>
+void	iter(T (*array)[N], std::size_t array_len, void (*f)(T&))
 {
 	for (std::size_t i = 0; i < array_len; i++)
 		f(array[i]);
 }
 
-template <typename T>
-void	iter(const T* array, std::size_t array_len, void (*f)(const T&))
+template <typename T, std::size_t N>
+void	iter(const T (*array)[N], std::size_t array_len, void (*f)(const T&))
 {
 	for (std::size_t i = 0; i < array_len; i++)
 		f(array[i]);
