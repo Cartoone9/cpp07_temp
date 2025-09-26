2239dd98d4e25edf753d2ec9c1c27ba3e09fdcab
jramiro <jramiro@student.42paris.fr>
2025-09-26 20:18:08 +0200

    ex01

--- ex01/hdrs/inter.hpp
+++ ex01/hdrs/inter.hpp
@@ -6,7 +6,7 @@
 /*   By: jramiro <jramiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:38:00 by jramiro           #+#    #+#             */
-/*   Updated: 2025/09/17 23:49:55 by jramiro          ###   ########.fr       */
+/*   Updated: 2025/09/26 20:17:25 by jramiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ template <typename T, std::size_t N>
 void	iter(T (*array)[N], std::size_t array_len, void (*f)(T&))
 {
 	for (std::size_t i = 0; i < array_len; i++)
-		f(array[i]);
+		f((*array)[i]);
 }
 
 template <typename T, std::size_t N>
 void	iter(const T (*array)[N], std::size_t array_len, void (*f)(const T&))
 {
 	for (std::size_t i = 0; i < array_len; i++)
-		f(array[i]);
+		f((*array)[i]);
 }
 
 #endif // #ifndef INTER_HPP
