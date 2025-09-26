183a722161f95e0efc963bcfbf3b29300bde7951
jramiro <jramiro@student.42paris.fr>
2025-09-26 20:56:12 +0200

    ex01

--- ex01/hdrs/iter.hpp
+++ ex01/hdrs/iter.hpp
@@ -6,12 +6,12 @@
 /*   By: jramiro <jramiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:38:00 by jramiro           #+#    #+#             */
-/*   Updated: 2025/09/26 20:17:25 by jramiro          ###   ########.fr       */
+/*   Updated: 2025/09/26 20:28:01 by jramiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INTER_HPP
-#define INTER_HPP
+#ifndef ITER_HPP
+#define ITER_HPP
 
 template <typename T, std::size_t N>
 void	iter(T (*array)[N], std::size_t array_len, void (*f)(T&))
@@ -27,4 +27,4 @@ void	iter(const T (*array)[N], std::size_t array_len, void (*f)(const T&))
 		f((*array)[i]);
 }
 
-#endif // #ifndef INTER_HPP
+#endif // #ifndef ITER_HPP
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,16 +6,69 @@
 /*   By: jramiro <jramiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 17:43:14 by jramiro           #+#    #+#             */
-/*   Updated: 2025/09/26 18:45:16 by jramiro          ###   ########.fr       */
+/*   Updated: 2025/09/26 20:55:17 by jramiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+#include "colors.hpp"
 #include "iter.hpp"
+
+template <typename T>
+void	printArray(T *array, std::size_t arr_size)
+{
+	for (std::size_t i = 0; i < arr_size; i++)
+		std::cout << array[i] << std::endl;
+}
+
+template <typename T>
+void	addOne(T& var)
+{
+	var++;
+}
 
 int	main(void)
 {
+	// -------------------------------------------------------------
+	std::cout << REVERSED "--- INT ARRAY ---\n" RESET << std::endl;
+
+	// array
+	int			array_int[] = {1, 2, 3, 4, 5};
+	std::size_t	arr_size = sizeof(array_int) / sizeof(array_int[0]);
+
+	// before
+	std::cout << UNDERLINE ORANGE "Before:" RESET << std::endl;
+	printArray(array_int, arr_size);
+
+	iter(&array_int, arr_size, addOne);
+
+	std::cout << std::endl;
+
+	// after
+	std::cout << UNDERLINE TEAL "After:" RESET << std::endl;
+	printArray(array_int, arr_size);
+
+
+	// -------------------------------------------------------------
+	std::cout << REVERSED "\n--- CHAR ARRAY ---\n" RESET << std::endl;
+
+	// array
+	char	array_char[] = {'a', 'b', 'c', 'd', 'e'};
+	arr_size = sizeof(array_char) / sizeof(array_char[0]);
+
+	// before
+	std::cout << UNDERLINE ORANGE "Before:" RESET << std::endl;
+	printArray(array_char, arr_size);
+
+	iter(&array_char, arr_size, addOne);
+
+	std::cout << std::endl;
+
+	// after
+	std::cout << UNDERLINE TEAL "After:" RESET << std::endl;
+	printArray(array_char, arr_size);
 
 	return (0);
 }
